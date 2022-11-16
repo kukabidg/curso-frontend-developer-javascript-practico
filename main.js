634c09e2d375487cff2149042ef09e68c0bdc525
AlexDavidAchina <achinadavi.d.esign@gmail.com>
2022-11-15 23:42:32 -0500

    Valida

--- main.js
+++ main.js
@@ -1,10 +1,63 @@
+/** Variables*/
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+/** constructor */
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon .addEventListener('click', toggleCarritoAside);
 
+/** Functions */
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+     }
     desktopMenu.classList.toggle('inactive');
   
 }
+function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+     }
+    mobileMenu.classList.toggle('inactive');
 
+}
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+ if(!isMobileMenuClosed){
+    mobileMenu.classList.add('inactive');    
+ }
+ desktopMenu.classList.add('inactive');
+ aside.classList.toggle('inactive');
+}
+
+const productList = [];
+productList.push({
+name: 'Bike',
+price: 120,
+imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+name: 'Iphone',
+price: 1200,
+imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+name: 'Tv',
+price: 720,
+imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+for (product of productList){
+    document.createElement();
+
+}
