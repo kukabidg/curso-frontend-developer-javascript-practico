a5bc4a987b032e8d75fec97ece0968d97372329c
AlexDavidAchina <achinadavi.d.esign@gmail.com>
2022-11-17 23:09:11 -0500

    Valida

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const mobileMenu = document.querySelector('.mobile-menu');
 
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer =document.querySelector('.cards-container');
 
 /** constructor */
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -57,7 +58,35 @@ price: 720,
 imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 });
 
-for (product of productList){
-    document.createElement();
 
+function renderProducts(arr){
+
+    for (product of arr){
+        const productCard = document.createElement('div');
+        const productImg = document.createElement('img');
+        productCard.classList.add('product-card');
+        productImg.setAttribute('src',product.imagen);
+        ////////
+        const productInfo = document.createElement('div');
+        const productInfoDiv = document.createElement('div');
+        const productInfoPrice = document.createElement('p');
+        const productInfoName = document.createElement('p');
+        productInfo.classList.add('product-info');
+        productInfoPrice.innerText = '$ ' + product.price;
+        productInfoName.innerText = product.name;
+        ///////
+        const productInfoFigure=document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+        //////
+        productInfoDiv.appendChild(productInfoPrice);
+        productInfoDiv.appendChild(productInfoName);
+        productInfoFigure.appendChild(productImgCart);
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+        cardsContainer.appendChild(productCard);
+    }
 }
+renderProducts(productList);
